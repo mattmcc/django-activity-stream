df990631ee92551515b4162f6d7a32d6b0f9d885
Matt McClanahan <matt@typethink.com>
2010-06-08 03:26:49 -0700

    Fix offset slicing for users_activity_stream, following_activity_stream, and global_activity_stream

--- activity_stream/templatetags/activity_stream_tags.py
+++ activity_stream/templatetags/activity_stream_tags.py
@@ -23,58 +23,41 @@ def following_him(context, user, count):
 
 
 @register.inclusion_tag("activity_stream/user_activity_stream.html", takes_context=True)
-def users_activity_stream(context, user, count, offset=0):
-	if not count:
-		count = 20
-		
-	if not offset:
-		offset=0
-	activity_items = ActivityStreamItem.objects.filter(actor=user, 
-						       subjects__isnull=False, 
-						       created_at__lte=datetime.datetime.now())\
-						.order_by('-created_at').distinct()[offset:count]
-	
-	return {"activity_items": activity_items,
-			"user": context.get("user"),
-			"request":context.get("request")
-			}
+def users_activity_stream(context, user, count=20, offset=0):
+    activity_items = ActivityStreamItem.objects.filter(actor=user,
+                            subjects__isnull=False,
+                            created_at__lte=datetime.datetime.now())\
+                        .order_by('-created_at').distinct()[offset:int(count)+int(offset)]
+
+    return {"activity_items": activity_items,
+            "user": context.get("user"),
+            "request":context.get("request")
+            }
 
 
 @register.inclusion_tag("activity_stream/friends_activity_stream.html", takes_context=True)
-def following_activity_stream(context, user, count, offset=0):
-	
-	if not count:
-		count = 20
-		
-	if not offset:
-		offset=0
-	
-	following =  get_people_i_follow(user, 1000)
-	following = list(following)
-	following.append(user)   
-	activity_items = ActivityStreamItem.objects.filter(actor__in=following, \
-													   subjects__isnull=False, \
-													   created_at__lte=datetime.datetime.now())\
-												.order_by('-created_at').distinct()[offset:count]
-	return {"activity_items": activity_items, "user": context.get("user"),
-		"request":context.get("request")}
+def following_activity_stream(context, user, count=20, offset=0):
+    following =  get_people_i_follow(user, 1000)
+    following = list(following)
+    following.append(user)
+    activity_items = ActivityStreamItem.objects.filter(actor__in=following, \
+                                                    subjects__isnull=False, \
+                                                    created_at__lte=datetime.datetime.now())\
+                                                .order_by('-created_at')\
+                                                .distinct()[offset:int(count)+int(offset)]
+
+    return {"activity_items": activity_items, "user": context.get("user"),
+        "request":context.get("request")}
 
 
 @register.inclusion_tag("activity_stream/global_activity_stream.html", takes_context=True)
-def global_activity_stream(context, count, offset=0, privacylevel=0):
-	
-	if not count:
-		count = 20
-		
-	if not offset:
-		offset=0
-		
-	activity_items = ActivityStreamItem.objects.filter(subjects__isnull=False,
-										created_at__lte=datetime.datetime.now())\
-								.order_by('-created_at').distinct()[offset:count]
-	
-	return {"activity_items": activity_items, "user": context.get("user"),
-		"request":context.get("request")}
+def global_activity_stream(context, count=20, offset=0, privacylevel=0):
+    activity_items = ActivityStreamItem.objects.filter(subjects__isnull=False,
+                                        created_at__lte=datetime.datetime.now())\
+                                .order_by('-created_at').distinct()[offset:int(count)+int(offset)]
+
+    return {"activity_items": activity_items, "user": context.get("user"),
+        "request":context.get("request")}
 
 
 class RenderActivityNode(Node):
